5c1cf37ed6ee10d314f6a6f2d24003df5f049f62
Bomi-R24 <buomkuothkok@yahoo.com>
2025-07-03 22:34:09 +0300

    Added a lightbulb feature to the website and a js file to control it.

---
+++ js/lightbulb.js
@@ -0,0 +1,7 @@
+function turnOn() {
+    document.getElementById("bulb").src = "../images/BulbOn.jpeg";
+}
+
+function turnOff() {
+    document.getElementById("bulb").src = "../images/BulbOff.jpeg";
+}
